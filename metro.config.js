8ca498142c95d894b4552aa434f3533d5fb3e552
jansparwath <jansparwath@gmail.com>
2025-10-23 16:08:37 +0000

    fix: implement comprehensive build solution

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Alias react-native-fs to expo-file-system
 config.resolver.extraNodeModules = {
   ...config.resolver.extraNodeModules,
   'react-native-fs': require.resolve('expo-file-system'),
